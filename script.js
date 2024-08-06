f45ea5259e4e7c1ee66eeb407163512c3930c2d1
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-08-06 19:51:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+const btn = document.getElementById('button')
+btn.addEventListner("click", (event)=>{
+
+	let name = document.getElementById('name').value;
+	let year = document.getElementById('year').value;
+
+	let url = document.getElementById('url').value;
+	url.textContent = `https://localhost:8080/? name=${name}&year=${year}`;
+	
+})
